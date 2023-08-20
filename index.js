bbbdeab9e44fe8c102832548851e54fbefb0c25b
douglasbrum <douglasbrums@hotmail.com>
2023-08-20 19:56:10 -0300

    refactor: update calculate from DB function
    General function with better naming enabling users to query anything

--- index.js
+++ index.js
@@ -42,9 +42,44 @@ const axios = require('axios');
                     });
                 }
             }
-
             resolve();
         });
+    };
+    
+    /* Returns the first element of a query */
+    const queryOne = async (customQuery) => {
+        try {
+            const result = await db.query(
+                customQuery,
+                {},
+                { single: true }
+                );
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    };
+    
+    /* Returns filtered object list from a flat JSON object where key matches a certain value. */
+    const filterFlat = (json, key, values) => {
+        return json.data.filter(item => values.includes(item[key]));
+    };
+    
+    /* Returns the sum of values that matches a property*/
+    const calculateObjectPropertySum = (objects, property) => {
+        return objects.map(item => item[property]).reduce((acc, value) => acc + value, 0);
+    };
+    
+    /* Returns the fetched data from source */
+    const fetchData = async (src) => {
+        try {
+            response = await axios.get(src);
+            return response.data;
+        }
+        catch (e) {
+            console.log(e.message);
+            return null;
+        }
     };
 
     //public
@@ -63,70 +98,35 @@ const axios = require('axios');
         });
     };
 
-    /* Returns filtered object list from a flat JSON object where key matches a certain value. */
-    const filterFlat = (json, key, values) => {
-        return json.data.filter(item => values.includes(item[key]));
-    };
-    
-    /* Returns the sum of values that matches a property*/
-    const calculateObjectPropertySum = (objects, property) => {
-        return objects.map(item => item[property]).reduce((acc, value) => acc + value, 0);
-    };
-
-    /* Returns the fetched data from source */
-    const fetchData = async (src) => {
-        try {
-            response = await axios.get(src);
-            return response.data;
-        }
-        catch (e) {
-            console.log(e.message);
-            return null;
-        }
-    };
-
     const calculatePopulationByYear = (populationJson, years) => {
         const relevantData = filterFlat(populationJson, 'ID Year', years);
         return calculateObjectPropertySum(relevantData, 'Population');
     };
 
-    const calculateTotalPopulationFromDb = async () => {
-        try {
-            const result = await db.query(`
-                SELECT SUM((item->>'Population')::INT) AS total_population 
-                FROM (
-                    SELECT jsonb_array_elements(doc_record->'data')::jsonb AS item 
-                    FROM ${DATABASE_SCHEMA}.api_data
-                ) AS subquery 
-                WHERE (item->>'ID Year')::INT IN (2020, 2019, 2018);`,
-                { build: true },
-                { single: true }
-            );
-            return parseInt(result.total_population);
-        } catch (error) {
-            throw error;
-        }
-    }
-
     try {
         await migrationUp();
 
         const populationJson = await fetchData('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+        await db[DATABASE_SCHEMA].api_data.insert({ doc_record: populationJson });
+
+        const populationObject = await queryOne(`
+            SELECT SUM((item->>'Population')::INT) AS total_population 
+            FROM (
+                SELECT jsonb_array_elements(doc_record->'data')::jsonb AS item 
+                FROM ${DATABASE_SCHEMA}.api_data
+                ) AS jsonItems
+            WHERE (item->>'ID Year')::INT IN (2020, 2019, 2018);
+        `);
+
+        const populationFromDB = + populationObject.total_population;
         const populationFromFetch = calculatePopulationByYear(populationJson, [2020, 2019, 2018]);
 
-        await db[DATABASE_SCHEMA].api_data.insert({
-            doc_record: populationJson
-        });
-
-        const totalPopulation = await calculateTotalPopulationFromDb();
-        
-        //Logs results
-        console.log(`Total population for the years 2020, 2019 and 2018.`)
+        console.log(`Total population sum for the years 2020, 2019 and 2018.`);
         console.log('NodeJS:',  populationFromFetch);
-        console.log('PostgreSQL:', totalPopulation);
+        console.log('PostgreSQL:', populationFromDB);
 
     } catch (e) {
-        console.log(e.message)
+        console.log(e.message);
     } finally {
         console.log('finally');
     }
